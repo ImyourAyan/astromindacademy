eebdd6cc8034398eef214dbf406cfac2e44b88b9
Ayan Banerjee <ayanbanerjee231@gmail.com>
2026-02-05 00:18:18 +0530

    Testing purpose only

---
+++ project-demo/js/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("testBtn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+    output.textContent = "✅ JavaScript is working!";
+});
+
